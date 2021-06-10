134b4f79c5374e60f9e240184f3308abcd9ab88a
Adem ilter <ademilter@gmail.com>
2021-06-10 12:30:36 +0300

    configration

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <NiceAvatar />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import NiceAvatar from "./components/NiceAvatar";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    NiceAvatar,
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
